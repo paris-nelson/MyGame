ddf51dcaaeb584cbb80825b438e734db95411c7c
paris-nelson <paris.t.nelson@gmail.com>
2022-11-07 22:45:04 -0600

    Tomorrow's todo

--- PT/src/Enums/MoveImpact.java
+++ PT/src/Enums/MoveImpact.java
@@ -2,4 +2,7 @@ package Enums;
 
 public enum MoveImpact {
 	EnemyOnly,FriendOnly,Self,Any
+	//TODO:now to actually add these impacts to the moveeffects file. need to manually determine for each effect whats right.
+	//for self effects,need to remove the target=self and add impact=self instead in the right place. This will also be a good
+	//point to finally go through movestrings and fix them up as well.
 }
